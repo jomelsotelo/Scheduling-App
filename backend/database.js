b866da5bd10d619d8a2a44850b6d179e0c76f1c1
Jomel Sotelo <119029800+jomelsotelo@users.noreply.github.com>
2023-10-22 14:30:08 -0700

    Set up the database up and running locally and added a database.js

---
+++ backend/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+})
+
+// Export it for use in other parts of your app
+module.exports = pool.promise()
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const database = require('./database');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/api/USERS', async (req, res) => {
+    try {
+        const [rows] = await database.query("SELECT username FROM user");
+        res.json(rows);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).send('Server Error');
+    }
+});cd
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

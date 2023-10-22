3b547866c2a135487e1c9950b8bc38f011204d29
Jomel Sotelo <119029800+jomelsotelo@users.noreply.github.com>
2023-10-21 20:37:44 -0700

    Added Minimal Test hello world in the backend and added concurrently to run frontend and backend server at the same time.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 

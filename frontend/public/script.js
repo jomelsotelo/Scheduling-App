de9bdf9e52d39e41b17c7a3e6e6860ac1a733b3f
Katy Chan <121604663+Kren1375@users.noreply.github.com>
2023-10-30 15:10:55 -0700

    Added Sign out button placeholder

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default BasicExample;
 //Testing js with two methods
 function webNote() {
     alert("This website is to schedule on a calendar!");
@@ -10,7 +44,7 @@ function callNote() {
 //logging in 
 //document.querySelector('#loginButton').addEventListener('click', login)
 
-document.getElementById("loginButton").onclick = function () {
+document.getElementById("loginOut").onclick = function () {
     location.replace("https://www.w3schools.com");
 }
 function login() {
@@ -18,7 +52,7 @@ function login() {
     //location.href = "/index.html";
     location.replace("index.html")
     //<a href="index.html"></a>
-
+    return true;
 }
 
 //Account verification for different username and password
@@ -26,3 +60,19 @@ function login() {
 function check() {
 
 }
+
+//Logout button Attempts
+function logOut() {
+    location.replace('login.html');
+    //onclick = "location.href='login.html'";
+    return true;
+}
+/*
+document.getElementById("logOut").onclick = function () { toLogin() };
+
+function toLogin() {
+    window.location.href = "login.html";
+};*/
+
+
+

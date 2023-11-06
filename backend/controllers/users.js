28a5e82d884e7ed383f77e7adc248cffeab24b61
Jose Vasquez <146392641+JoseVas2003@users.noreply.github.com>
2023-11-06 11:29:24 -0800

    finished connecting the user.js to the database

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,8 @@
 import database from '../config/database.js';
 
 
+
+
 //gets all users
 export const getUsers = async (req, res) => {
   try {
@@ -11,7 +13,6 @@ export const getUsers = async (req, res) => {
         res.status(500).send('Server Error');
   }
 }
-
 
 
 
@@ -35,56 +36,79 @@ export const getUser = async(req, res) => {
 
 //Create a new user
 export const createUser = async (req, res) => {
-  const user = req.body;
-  // Validate user data here
-  try {
-      // Insert user into the database
-      const query = "INSERT INTO users (first_name, last_name, email, salt, password_hash) VALUES (?, ?, ?, ?, ?)";
-      const values = [user.first_name, user.last_name, user.email, user.salt, user.password_hash];
-     
-      const result = await database.query(query, values);
+  const user = req.body; // Assuming user data is in the request body
 
 
-      if (result.affectedRows === 1) {
-          res.send(`User with email ${user.email} added`);
-      } else {
-          res.status(500).send('Failed to create a user.');
-      }
-  } catch (error) {
-      console.error("Error creating user:", error);
-      res.status(500).send('Server Error');
+  // Check if the user object contains the required fields
+  if (
+    !user.first_name ||
+    !user.last_name ||
+    !user.email ||
+    !user.salt ||
+    !user.password_hash
+  ) {
+    return res.status(400).send('All required fields must be provided.');
   }
-}
+
+
+  try {
+    const query = `INSERT INTO users (first_name, last_name, email, salt, password_hash)
+                   VALUES (?, ?, ?, ?, ?)`;
+    const values = [
+      user.first_name,
+      user.last_name,
+      user.email,
+      user.salt,
+      user.password_hash,
+    ];
+    const result = await database.query(query, values);
+
+
+    //instead of printing  user has been added it outputs failed to create user
+    if (result.affectedRows === 0) {
+ 
+      res.status(500).send('Failed to create a user.');
+    } else {
+
+
+  res.send(`User with email ${user.email} added`);
+    }
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send('Server Error');
+  }
+};//updates a user with put
 
 
 
 
-//updates a user with put
 export const updateUser = async (req, res) => {
   const { id } = req.params;
-  const { Fname, Lname, age } = req.body;
-  const contentType = req.get('Content-Type'); // Get the content type from headers
+  const { first_name, last_name, email, salt, password_hash } = req.body;
 
 
-  if (!contentType || contentType !== 'application/json') {
-      return res.status(415).send('Unsupported Media Type. Please send data in JSON format.');
+  // Check if the user object contains the required fields
+  if (!first_name || !last_name || !email || !salt || !password_hash) {
+    return res.status(400).send('All required fields must be provided.');
   }
 
 
   try {
-      // Update user data in the database
-      const result = await database.query("UPDATE users SET Fname = ?, Lname = ?, age = ? WHERE id = ?", [Fname, Lname, age, id]);
+    // Update user data in the database
+    const query = `UPDATE users SET first_name = ?, last_name = ?, email = ?, salt = ?, password_hash = ? WHERE user_id = ?`;
+    const values = [first_name, last_name, email, salt, password_hash, id];
+    const result = await database.query(query, values);
 
 
-      if (result.affectedRows === 0) {
-          return res.status(404).send(`User with ID ${id} not found.`);
-      }
+    if (result.affectedRows === 0) {
+      return res.status(404).send(`User with ID ${id} not found.`);
+    }
 
 
-      res.status(200).send(`User with ID ${id} has been updated.`);
+    res.status(200).send(`User with ID ${id} has been updated.`);
   } catch (error) {
-      console.error("Error updating user:", error);
-      res.status(500).send('Server Error');
+    console.error("Error updating user:", error);
+    res.status(500).send('Server Error');
   }
 }
 
@@ -92,10 +116,8 @@ export const updateUser = async (req, res) => {
 //Deletes user with delete
 export const deleteUser = async (req, res) => {
   const { id } = req.params;
-
-
   try {
-      const result = await database.query("DELETE FROM users WHERE id = ?", [id]);
+      const result = await database.query("DELETE FROM users WHERE user_id = ?", [id]);
 
 
       if (result.affectedRows === 0) {
@@ -112,20 +134,30 @@ export const deleteUser = async (req, res) => {
 //USER MANAGEMENT
 
 
+
+
 // //Create a new user.
 // createUser
-INSERT INTO 
+
+
+
 
 // //Retrieve user profile information.
 // getUserProfile
+
+
 
 
 // //Retrieve all user data.
 // getUserData
 
 
+
+
 // //Edit user info.
 // editUserInfo
+
+
 
 
 // //Delete user.

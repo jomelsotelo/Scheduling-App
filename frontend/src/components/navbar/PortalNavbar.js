2570b977a1f684060de4d650db80aceaef127473
Jomel Sotelo <119029800+jomelsotelo@users.noreply.github.com>
2023-11-15 15:34:55 -0800

    Added feature to add availability -Not fully complete

--- frontend/src/components/navbar/PortalNavbar.js
+++ frontend/src/components/navbar/PortalNavbar.js
@@ -1,11 +1,46 @@
-import React from "react";
-import { Button, Nav } from "react-bootstrap";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState, useEffect} from "react"
+import { Button, Nav } from "react-bootstrap"
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
 import { useNavigate } from "react-router-dom"
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 const PortalNavbar = () => {
     const navigate = useNavigate()
+    const [user, setUser] = useState(null)
+    const { id } = useParams()
     
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            // Get the token from local storage
+            const token = localStorage.getItem('user-token')
+    
+            if (token) {
+              // Send a request to the server to get user data
+              const response = await axios.get('/api/users/${id}', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+    
+              // Assuming the server responds with user data
+              const userData = response.data;
+    
+              // Update the state to store the user data
+              setUser(userData)
+            }
+          } catch (error) {
+            console.error('Error fetching user data:', error);
+            // Handle error
+          }
+        }
+    
+        // Call the function to fetch user data when the component mounts
+        fetchUserData()
+      }, [id])
+
     const logout = () => {
         localStorage.clear()
         navigate('/auth/login')
@@ -26,6 +61,9 @@ const PortalNavbar = () => {
                 <Nav.Link onClick={navigateToCalendar} style={{ fontSize: '1rem', margin: '5px 5px' }}>Calendar</Nav.Link>
                 </Nav>
                 <Container />
+                <Navbar.Text>
+                    Signed in as: {user?.first_name || 'Guest'}
+                </Navbar.Text>
                 <Nav className="ml-auto">
                     <Nav.Link>
                         <Button className="btn-secondary" onClick={logout}>Log Out</Button>
--- frontend/src/pages/portal/Calendar.js
+++ frontend/src/pages/portal/Calendar.js
@@ -1,17 +1,53 @@
 import React, { useState } from 'react'
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 import dayjs from 'dayjs'
-import AvailiabilityForm from '../../components/AvailabilityForm'
 
 const localizer = dayjsLocalizer(dayjs)
 
 const MyCalendar = (props) => {
   const [myEventsList, setMyEventsList] = useState([])
+  const [selectedSlot, setSelectedSlot] = useState(null)
 
   // Function to handle adding availability
-  const handleAddAvailability = (newEvent) => {
-    setMyEventsList([...myEventsList, newEvent])
-  }
+  const handleSelectSlot = (slotInfo) => {
+    const { start, end, action } = slotInfo;
+
+    // Check the current view to determine how to handle the selection
+    if (action === 'select') {
+      if (props.view === 'month') {
+        // In month view, select the whole day
+        setSelectedSlot({ start: dayjs(start).startOf('day'), end: dayjs(end).endOf('day') });
+      } else {
+        // In day/week view, select the specific time
+        setSelectedSlot({ start, end });
+      }
+    } else if (action === 'click') {
+      // Handle click events if needed
+    }
+  };
+
+  const handleConfirmSelection = () => {
+    if (selectedSlot) {
+      const newEvent = {
+        title: 'Available',
+        start: selectedSlot.start,
+        end: selectedSlot.end,
+      };
+
+      setMyEventsList([...myEventsList, newEvent]);
+      setSelectedSlot(null); // Clear the selected slot
+    }
+  };
+  
+  const handleCancelSelection = () => {
+    setSelectedSlot(null); // Clear the selected slot
+  };
+
+  const handleRemoveAvailability = (event) => {
+    const updatedEventsList = myEventsList.filter((e) => e !== event);
+    setMyEventsList(updatedEventsList);
+  };
+
 
   return (
     <div>
@@ -21,10 +57,32 @@ const MyCalendar = (props) => {
         startAccessor="start"
         endAccessor="end"
         selectable
-        onSelectSlot={(slotInfo) => handleAddAvailability(slotInfo)}
+        onSelectSlot={handleSelectSlot}
         style={{ height: 500 }}
       />
-      <AvailiabilityForm onAddAvailability={handleAddAvailability} />
+      {selectedSlot && (
+        <div>
+          <p>Selected Slot:</p>
+          <p>Start: {selectedSlot.start.toLocaleString()}</p>
+          <p>End: {selectedSlot.end.toLocaleString()}</p>
+          <button onClick={handleConfirmSelection}>Confirm</button>
+          <button onClick={handleCancelSelection}>Cancel</button>
+        </div>
+      )}
+
+      <div>
+        <p>Drag your cursor to select your available time.</p>
+        <p>Select Week or Day to pick a specific time.</p>
+      </div>
+      <div>
+        <p>Selected Availabilities:</p>
+        {myEventsList.map((event, index) => (
+          <div key={index}>
+            {event.title} - {event.start.toLocaleString()} to {event.end.toLocaleString()}
+            <button onClick={() => handleRemoveAvailability(event)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

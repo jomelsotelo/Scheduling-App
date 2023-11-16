e918a98f1643c736ed31e300fa37d2bacb7ca999
Jose Vasquez <146392641+JoseVas2003@users.noreply.github.com>
2023-11-16 15:46:00 -0800

    fixed the availability

--- backend/controllers/availability.js
+++ backend/controllers/availability.js
@@ -1,57 +1,122 @@
 import database from '../config/database.js'
 
+
 //Create a new availability entry for a user.
 export const createAvailability = async (req, res) => {
-    const { user_id, start_time, end_time } = req.body
-    const query = 'INSERT INTO user_availability (user_id, start_time, end_time) VALUES (?, ?, ?)'
-    database.query(query, [user_id, start_time, end_time], (err, results) => {
-      if (err) {
-        console.log('Error creating availability:', err)
-        res.status(500).json({ error: 'Internal Server Error' })
-      } else {
-        res.status(201).json({ message: 'Availability created successfully', id: results.insertId })
-      }
-    })
+  try {
+    const { user_id, start_time, end_time } = req.body;
+    const query = 'INSERT INTO user_availability (user_id, start_time, end_time) VALUES (?, ?, ?)';
+    const results = await database.query(query, [user_id, start_time, end_time]);
+
+
+    if (results && results.insertId) {
+       res.status(500).json({ error: 'Failed to create availability' });
+    } else {
+     
+res.status(201).json({ message: 'Availability created successfully', id: results.insertId });
+
+
+    }
+  } catch (error) {
+    console.error('Error creating availability:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-//Retrieve all availabilities for a user.
+};
+
+
+// Retrieve all availabilities for a user
 export const getAvailability = async (req, res) => {
-    const userId = req.params.user_id
-    const query = 'SELECT * FROM user_availability WHERE user_id = ?'
-    database.query(query, [userId], (err, results) => {
-      if (err) {
-        console.log('Error retrieving availabilities:', err)
-        res.status(500).json({ error: 'Internal Server Error' })
-      } else {
-        res.status(200).json(results)
-      }
-    })
+  const { id } = req.params;
+  try {
+    const [rows] = await database.query("SELECT * FROM user_availability WHERE user_id = ?", [id]);
+
+
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching user availabilities:", error);
+    res.status(500).send('Server Error');
   }
+};
+
+
+// Retrieve a specific availability from a user
+export const getSpecificAvailability = async (req, res) => {
+  try {
+    const userId = req.params.user_id;
+    const availabilityId = req.params.availability_id;
+
+
+    const query = 'SELECT * FROM user_availability WHERE user_id = ? AND availability_id = ?';
+
+
+    const [result] = await database.query(query, [userId, availabilityId]);
+
+
+    if (result && result.length > 0) {
+      res.status(404).json({ error: 'Availability not found' });
+
+
+    } else {
+            res.status(200).json({ message: 'Availability retrieved successfully', data: result });
+
+
+    }
+  } catch (error) {
+    console.error('Error retrieving availability:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+
+
 
 //Update an existing availability entry.
 export const updateAvailability = async (req, res) => {
-    const availabilityId = req.params.id
-    const { start_time, end_time } = req.body
-    const query = 'UPDATE user_availability SET start_time = ?, end_time = ? WHERE id = ?'
-    database.query(query, [start_time, end_time, availabilityId], (err) => {
-      if (err) {
-        console.log('Error updating availability:', err)
-        res.status(500).json({ error: 'Internal Server Error' })
-      } else {
-        res.status(200).json({ message: 'Availability updated successfully' })
-      }
-    })
+  try {
+    const availabilityId = req.params.id;
+    const { start_time, end_time } = req.body;
+    const query = 'UPDATE user_availability SET start_time = ?, end_time = ? WHERE availability_id = ?';
+
+
+    const result = await database.query(query, [start_time, end_time, availabilityId]);
+
+
+    if (result.affectedRows > 0) {
+      res.status(404).json({ error: 'Availability not found' });
+
+
+    } else {
+      res.status(200).json({ message: 'Availability updated successfully' });
+
+
+    }
+  } catch (error) {
+    console.error('Error updating availability:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};
+
 
 //Delete an availability entry.
 export const deleteAvailability = async (req, res) => {
-    const availabilityId = req.params.id
-    const query = 'DELETE FROM user_availability WHERE id = ?'
-    database.query(query, [availabilityId], (err) => {
-      if (err) {
-        console.log('Error deleting availability:', err)
-        res.status(500).json({ error: 'Internal Server Error' })
-      } else {
-        res.status(200).json({ message: 'Availability deleted successfully' })
-      }
-    })
+  try {
+    const availabilityId = req.params.id;
+   
+    const deleteQuery = 'DELETE FROM user_availability WHERE availability_id = ?';
+    const result = await database.query(deleteQuery, [availabilityId]);
+
+
+    if (result.affectedRows > 0) {
+      res.status(404).json({ error: 'Availability not found' });
+
+
+    } else {
+      res.status(200).json({ message: 'Availability deleted successfully' });
+
+
+    }
+  } catch (error) {
+    console.error('Error deleting availability:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};
--- backend/routes/availability.js
+++ backend/routes/availability.js
@@ -1,18 +1,14 @@
+
 import express from 'express'
-import { createAvailability, getAvailability, updateAvailability, deleteAvailability } from '../controllers/availability.js'
+import { createAvailability, getAvailability, getSpecificAvailability, updateAvailability, deleteAvailability } from '../controllers/availability.js'
+
 
 const router = express.Router()
-
-//This is used to create a new availability for a specific user.
 router.post('/:id/availability', createAvailability)
-
-//This is used to retrieve (get) the availabilities of a specific user.
 router.get('/:id/availability', getAvailability)
-
-//This is used to update the availabilities of a specific user.
+router.get('/:id/availability/:id',  getSpecificAvailability)
 router.put('/:id/availability/:id', updateAvailability)
-
-//This is used to delete a specific availability of a specific user.
 router.delete('/:id/availability/:id', deleteAvailability)
+
 
 export default router

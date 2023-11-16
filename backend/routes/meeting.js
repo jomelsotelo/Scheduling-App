b1ff942de13088fe96eeb07c27ca284bc83aa4ca
Angel Ramirez <146392572+AngelinhoRG@users.noreply.github.com>
2023-11-15 17:51:02 -0800

    Added comments to meeting.js to inform about the behavior of each method

--- backend/routes/meeting.js
+++ backend/routes/meeting.js
@@ -2,10 +2,20 @@ import express from 'express'
 import { createMeeting, getMeetingDetails, getAllMeetings, updateMeeting, deleteMeeting } from '../controllers/meeting.js'
 
 const router = express.Router()
+
+//This is used to create a meeting.
 router.post('/', createMeeting)
+
+//This is used to retrieve (get) the details of a specific meeting.
 router.get('/:id', getMeetingDetails)
+
+//This is used to get the information of all the meetings.
 router.get('/', getAllMeetings)
+
+//Updates meeting information. Allows for addition or deletion of participants.
 router.put('/:id', updateMeeting)
+
+//Deletes a meeting with specific id number.
 router.delete('/:id', deleteMeeting)
 
 export default router

d65e067cb8a27a39bbca68955f92b1adf9b20e75
hayabes <146310445+hyabes@users.noreply.github.com>
2023-12-01 15:24:12 -0800

    Able to pull basic info (name and email) now from backend onto user page. Will make look nicer soon. Posted soley for other users test.

--- frontend/src/pages/portal/UserInfo.js
+++ frontend/src/pages/portal/UserInfo.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
-const UserInfo = ({ user }) => {
+const UserInfo = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Grabs user data
+                const token = localStorage.getItem('user-token');
+                const axiosInstance = axios.create({
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const decodedToken = jwtDecode(token);
+                const userId = decodedToken.userId;
+
+                const userDataResponse = await axiosInstance.get(`/api/users/${userId}`);
+
+                const userData = userDataResponse.data;
+
+                setUser(userData);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <h2>User Information</h2>
-                
                 <Link to="/account">
-          <Button className="button-link" variant="primary" size="lg">
-          Edit Info
-          </Button>{' '}
-        </Link>
-
+                    <Button className="button-link" variant="primary" size="lg">
+                        Edit Info
+                    </Button>{' '}
+                </Link>
             </div>
-            <p>Name: {user ? `${user.first_name} ${user.last_name}` : 'Guest'}</p>
+            <p>Name: {user ? `${user.first_name} ${user.last_name}` : 'N/A'}</p>
             <p>Email: {user ? user.email : 'N/A'}</p>
             {/* MORE INFO HERE */}
         </div>

a70faf8dd5006390b28028c51119dbce8c51a21b
Jose Vasquez <146392641+JoseVas2003@users.noreply.github.com>
2023-12-06 20:19:28 -0800

    fixed timslots to connect to calender

--- backend/controllers/timeslots.js
+++ backend/controllers/timeslots.js
@@ -1,33 +1,48 @@
-import database from '../config/database.js'
+import database from '../config/database.js';
 
-//Retrieve available timeslots for scheduling a meeting with one or more users.
+// Retrieve common availability slots for a list of user IDs
 export const getAvailableTime = async (req, res) => {
-    const { users, duration, date } = req.body
-  
-    // Ensure that users, duration, and date are provided
-    if (!users || !duration || !date) {
-      return res.status(400).json({ error: 'Users, duration, and date are required parameters' })
+  try {
+    const users = req.body;
+
+    // Extract user_ids from the array of user objects and flatten the array
+    const userIDs = users.map(user => user.user_ids).flat();
+    console.log(userIDs);
+
+    // Ensure userIDs is an array and has at least two elements
+    if (!Array.isArray(userIDs) || userIDs.length < 2) {
+      res.status(400).json({ error: 'Invalid user IDs provided' });
+      return;
     }
 
-    const query = ' SELECT user_id, start_time, end_time FROM user_availability WHERE user_id IN (?) AND start_time >= ? AND end_time <= ? AND NOT EXISTS ( SELECT 1 FROM meetings WHERE (start_time >= ? AND start_time < ?) OR (end_time > ? AND end_time <= ?))'
-  
-    // Calculate end time based on duration
-    const endTime = new Date(date)
-    endTime.setMinutes(endTime.getMinutes() + duration)
-  
-    // Prepare user IDs for the query
-    const userIds = users.map((user) => user.id)
-  
-    database.query(
-      query,
-      [userIds, date, endTime, date, endTime, date, endTime],
-      (err, results) => {
-        if (err) {
-          console.log('Error searching for available timeslots:', err)
-          res.status(500).json({ error: 'Internal Server Error' })
-        } else {
-          res.status(200).json(results)
-        }
+    // Generate the SQL statement dynamically with the user IDs
+    const sqlStatement = `
+      SELECT DISTINCT ua1.start_time, ua1.end_time
+      FROM user_availability ua1
+      JOIN user_availability ua2 ON ua1.user_id <> ua2.user_id
+                               AND ua1.start_time < ua2.end_time
+                               AND ua1.end_time > ua2.start_time
+      WHERE ua1.user_id IN (${userIDs.join(', ')})
+        AND ua2.user_id IN (${userIDs.join(', ')});
+    `;
+
+    // Execute the SQL query
+    try {
+      const [rows] = await database.query(sqlStatement);
+
+      // Send the response based on the result
+      if (rows.length === 0) {
+        res.status(404).json({ error: 'No common availabilities found' });
+      } else {
+        // Continue with the remaining logic or send the common availabilities in the response
+        res.status(200).json({ commonAvailabilities: rows });
       }
-    )
+    } catch (error) {
+      console.error("Error executing SQL query:", error);
+      res.status(500).send('Server Error');
+    }
+  } catch (error) {
+    console.error('Error processing user availabilities:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+};

e2514875e0a059232babd5c2b6a534e7c28c57bd
Jenica Chu <146392347+cixneja@users.noreply.github.com>
2023-12-12 01:24:38 -0800

    The edit account page now works

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
 import Button from 'react-bootstrap/Button';
 
-function EditAccount() {
-  const [user, setUser] = useState(null);
-  const { id } = useParams(); // Get the user ID from the URL
-  const navigate = useNavigate();
-
+function AccountInfo() {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -18,44 +13,7 @@ function EditAccount() {
   const [isLoading, setLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    // Call the function to fetch user data when the component mounts
-    fetchUserData();
-  }, []);
-
-  const fetchUserData = async () => {
-    try {
-      // Gets the token from local storage
-      const token = localStorage.getItem("user-token");
-
-      if (token) {
-        const axiosInstance = axios.create({
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const decodedToken = jwtDecode(token);
-        const userId = decodedToken.userId;
-
-        const userDataResponse = await axiosInstance.get(`/api/users/${userId}`);
-        const userData = userDataResponse.data;
-
-        // Updates the state to store the user data
-        setUser(userData);
-        console.log(user.userId);
-        setFormData({
-          firstName: userData.firstName,
-          lastName: userData.lastName,
-          password: userData.password,
-        });
-      }
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      // Handle error
-    }
-  };
+  const [userId, setUserId] = useState(null); // State to store user ID
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -65,49 +23,55 @@ function EditAccount() {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const editUser = {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      password: formData.password,
-    };
-
-    setLoading(true);
-
-    // Perform account update actions (e.g., API call)
-    axios.put(`/api/user/${id}`, editUser)
-      .then((response) => {
-        console.log("Edit was success:", response.data);
-        setSuccessMessage("Account edited successfully!");
-        setErrorMessage(""); // Clears any previous error messages
-        setTimeout(() => {
-          setLoading(false);
-          navigate(`/user/${id}`); // Redirect to user profile after successful edit
-        }, 2000);
-      })
-      .catch((error) => {
-        console.error("Edit error:", error);
-
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.error("Response data:", error.response.data);
-          console.error("Response status:", error.response.status);
-          console.error("Response headers:", error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.error("No response received from the server.");
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.error("Error setting up the request:", error.message);
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const token = localStorage.getItem("user-token");
+        if (!token) {
+          console.error("User token not found");
+          return;
         }
 
-        setSuccessMessage(""); // Clear any previous success messages
-        setErrorMessage("Edit failed. Please try again.");
-        setLoading(false);
+        const decodedToken = jwtDecode(token);
+        const userIdFromToken = decodedToken.userId; // Assuming the token has userId information
+
+        setUserId(userIdFromToken);
+      } catch (error) {
+        console.error("Error fetching user ID:", error);
+      }
+    };
+
+    fetchUserId();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const updatedData = {
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+        password: formData.password,
+      };
+
+      const token = localStorage.getItem("user-token");
+      const response = await axios.put(`/api/users/${userId}`, updatedData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
+
+      console.log("Update success:", response.data);
+      setSuccessMessage("Account information updated successfully!");
+      setErrorMessage(""); // Clear any previous error messages
+      setLoading(false);
+    } catch (error) {
+      console.error("Update error:", error);
+      setSuccessMessage(""); // Clear any previous success messages
+      setErrorMessage("Update failed. Please try again.");
+      setLoading(false);
+    }
   };
 
   return (
@@ -129,24 +93,31 @@ function EditAccount() {
         />
         <input
           type="password"
-          placeholder="Password"
+          placeholder="New Password"
           name="password"
           value={formData.password}
           onChange={handleInputChange}
         />
-
         <Button
           variant="primary"
           type="submit"
           disabled={isLoading}
         >
-          {isLoading ? 'Editing Account...' : 'Save Account Info'}
+          {isLoading ? 'Updating...' : 'Update'}
         </Button>
       </form>
-      {successMessage && <p className="success-message" style={{ color: 'green' }}>{successMessage}</p>}
-      {errorMessage && <p className="error-message" style={{ color: 'red' }}>{errorMessage}</p>}
+      {successMessage && (
+        <p className="success-message" style={{ color: 'green' }}>
+          {successMessage}
+        </p>
+      )}
+      {errorMessage && (
+        <p className="error-message" style={{ color: 'red' }}>
+          {errorMessage}
+        </p>
+      )}
     </div>
   );
 }
 
-export default EditAccount;
+export default AccountInfo;
